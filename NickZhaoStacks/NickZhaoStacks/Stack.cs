772d5f1bee331f20249e0d87f8504ea5e44de73f
Nick Zhao <nickzrz@hotmail.com>
2018-03-25 13:25:26 -0700

    Initial Commit

---
+++ NickZhaoStacks/NickZhaoStacks/Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NickZhaoStacks
+{
+    class Stack<T>
+    {
+        bool IsEmpty = true;
+        Stack<T> top = new Stack<T>();
+        public Stack()
+        {
+
+        }
+
+        void Push(T value)
+        {
+            if(IsEmpty)
+            {
+
+            }
+        }
+    }
+}
